e320ba58455e299add7d381d1517707f5c89308c
Arkhipov Danil <anfobox@gmail.com>
2021-05-29 19:19:25 +0700

    Add NewClass

--- 1Lab/In_Transport.cpp
+++ 1Lab/In_Transport.cpp
@@ -1,18 +1,22 @@
 #include "Transport.h"
 #include "Airplane.h"
 #include "Train.h"
+#include "Ship.h"
 
 Transport* Transport::In(ifstream& ifst) {
     Transport* T;
     int K;
 
     ifst >> K;
-    
+
     if (K == 1) {
         T = new Airplane;
     }
     else if (K == 2) {
         T = new Train;
+    }
+    else if (K == 3) {
+        T = new Ship;
     }
     else {
         return 0;
